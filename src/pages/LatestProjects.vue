a93e047d23b1a46ca1ce981818554b8d39651874
ippoRulez <ippolitodonato@gmail.com>
2023-02-17 19:40:55 +0100

    added a latest projects component

---
+++ src/pages/LatestProjects.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+import {store} from '../store';
+
+import ProjectCard from '../components/ProjectCard.vue';
+
+export default {
+	name: 'LatestProjects',
+
+	components: {ProjectCard},
+
+	data() {
+		return {
+			projects: [],
+		};
+	},
+
+	methods: {},
+
+	mounted() {
+		axios
+			.get(store.urlBackend + '/api/projects', {
+				params: {
+					last3: true,
+				},
+			})
+			.then((resp) => {
+				this.projects = resp.data;
+			});
+	},
+};
+</script>
+
+<template>
+	<div class="row">
+		<div v-for="project in projects" class="col-4">
+			<ProjectCard :project="project"></ProjectCard>
+		</div>
+	</div>
+</template>
+
+<style lang=""></style>
